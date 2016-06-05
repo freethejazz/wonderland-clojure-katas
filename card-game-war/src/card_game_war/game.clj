c8a8a41f0e25e8312a7c54f796423f81f000761d
Jonathan Freeman <freethejazz@gmail.com>
2016-06-04 19:49:58 -0500

    1...2...3...

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -3,11 +3,65 @@
 ;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+(def named-values {:jack 11
+                  :queen 12
+                  :king 13
+                  :ace 14})
+
+(def suit-boosters {:spade 0
+                    :club 0.1
+                    :diamond 0.2
+                    :heart 0.3})
 (def cards
   (for [suit suits
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn name->value [a-keyword]
+  (a-keyword named-values))
 
-(defn play-game [player1-cards player2-cards])
+(defn get-value [card]
+  (if (number? (second card))
+    (+ (second card) ((first card) suit-boosters))
+    (+ (name->value (second card)) ((first card) suit-boosters))))
+
+(defn fix-decks
+  "When the deck is exhausted, this will shuffle the discard
+  pile and make it the deck."
+  [start end]
+  (if (empty? start)
+    (do (println "===== erryday I'm shuffling =====") [(shuffle end) []])
+    [start end]))
+
+;; Convenience mappings for readability
+(def draw-from first)
+(def discard-into concat)
+
+(defn play-round
+  "Takes two cards, returns a vector of cards to add to the
+  corresponding decks that the two cards came from."
+  [player1-card player2-card]
+  (println "\n\nStarting Round:")
+  (println "comparing" player1-card "with" player2-card)
+  (if (> (get-value player1-card) (get-value player2-card))
+    (do (println "player1-card wins") [[player1-card, player2-card], []])
+    (do (println "player2-card wins") [[], [player1-card, player2-card]])))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [p1-draw-pile player1-cards
+         p1-discard-pile []
+         p2-draw-pile player2-cards
+         p2-discard-pile []]
+    (let [[p1-draw-pile, p1-discard-pile] (fix-decks p1-draw-pile p1-discard-pile)
+          _ (println "p1 decks" (count p1-draw-pile) (count p1-discard-pile))
+          [p2-draw-pile, p2-discard-pile] (fix-decks p2-draw-pile p2-discard-pile)
+          _ (println "p2 decks" (count p2-draw-pile) (count p2-discard-pile))]
+      (cond
+        (empty? p1-draw-pile) "Player 1 Lost!"
+        (empty? p2-draw-pile) "Player 2 Lost!"
+        :else (let [results (play-round (draw-from p1-draw-pile) (draw-from p2-draw-pile))]
+                (recur
+                  (rest p1-draw-pile)
+                  (discard-into p1-discard-pile (first results))
+                  (rest p2-draw-pile)
+                  (discard-into p2-discard-pile (second results))))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -5,15 +5,44 @@
 
 ;; fill in  tests for your game
 (deftest test-play-round
+  (def two-spade [:spade 2])
+  (def three-spade [:spade 3])
+  (def jack-spade [:spade :jack])
+  (def queen-spade [:spade :queen])
+  (def king-spade [:spade :king])
+  (def ace-spade [:spade :ace])
+
+  (def two-club [:club 2])
+  (def two-heart [:heart 2])
+  (def two-diamond [:diamond 2])
+
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= [[] [two-spade three-spade]] (play-round two-spade three-spade))))
+
+  (testing "queens are higher rank than jacks"
+    (is (= [[] [jack-spade queen-spade]] (play-round jack-spade queen-spade))))
+
+  (testing "kings are higher rank than queens"
+    (is (= [[] [queen-spade king-spade]] (play-round queen-spade king-spade))))
+
+  (testing "aces are higher rank than kings"
+    (is (= [[] [king-spade ace-spade]] (play-round king-spade ace-spade))))
+
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= [[] [two-spade two-club]] (play-round two-spade two-club))))
+
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= [[] [two-club two-diamond]] (play-round two-club two-diamond))))
+
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= [[] [two-heart two-heart]] (play-round two-heart two-heart)))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (def shuffledDeck (shuffle cards))
+  (def player1 (take 26 shuffledDeck))
+  (def player2 (drop 26 shuffledDeck))
+
+  (testing "the player loses when they run out of cards"
+    (is (= "Player 1 Lost!" (play-game [] player2)))
+    (is (= "Player 2 Lost!" (play-game player1 [])))))
 
